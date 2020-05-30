0bf4ae8e5bd87113c0db48f2c3ec829867f6b1dd
Matthew Elwin <elwin@northwestern.edu>
2020-05-29 23:17:40 -0500

    Working on debug and release buidls for different compilers to all coexist.

--- tivaware/TivaWareConfig.cmake
+++ tivaware/TivaWareConfig.cmake
@@ -6,10 +6,10 @@
 set(DRIVERLIB_DEBUG OFF CACHE BOOL "If ON, link against the debugging version of driverlib")
 
 if(DRIVERLIB_DEBUG)
-  include("${CMAKE_CURRENT_LIST_DIR}/TivaWareTargets_Debug.cmake")
+  include("${CMAKE_CURRENT_LIST_DIR}/Debug_${CMAKE_C_COMPILER_ID}/TivaWareTargets.cmake")
   add_library(driverlib alias driverlib_Debug)
 else()
-  include("${CMAKE_CURRENT_LIST_DIR}/TivaWareTargets_Release.cmake")
+  include("${CMAKE_CURRENT_LIST_DIR}/Release_${CMAKE_C_COMPILER_ID}/TivaWareTargets.cmake")
   add_library(driverlib alias driverlib_Release)
 endif()
 
