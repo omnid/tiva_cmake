b43fd18ca727e9f70dd7a943b6bcbe98f46866af
Matthew Elwin <elwin@northwestern.edu>
2020-05-31 00:01:09 -0500

    The example compiles with the ti compiler. Has not been tested.

--- tivaware/TivaWareConfig.cmake
+++ tivaware/TivaWareConfig.cmake
@@ -35,7 +35,6 @@ else()
     message(WARNING "TivaWare does not support ${CMAKE_SYSTEM_PROCESSOR}, use at your own risk!")
 endif()
 
-target_compile_definitions(driverlib INTERFACE $<$<C_COMPILER_ID:TI>, ccs>)
 
 #[[======================== Works Cited ======================================================
 [1] "TivaWare Peripheral Driver Library: User's Guide", Literature Number SPMU298E
