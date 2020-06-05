d9acdc737dc48f1d598527c7d9a9367018c2f76b
Matthew Elwin <elwin@northwestern.edu>
2020-06-04 23:58:39 -0500

    Set the prefix path properly in Preload.
    The project now seems to fully work in template mode (out of the build directory).
    It is also now designed better to work when installed to the system as well.

--- src/PreLoad.cmake
+++ src/PreLoad.cmake
@@ -7,11 +7,11 @@
 set(DEFAULT_TOOLCHAIN tiva-toolchain.cmake)
 
 # Detect if tiva_cmake is installed 
-find_package(TivaCMake QUIET)
-if(NOT TivaCMAKE_FOUND)
+find_package(TivaToolchain QUIET)
+if(NOT TivaToolchain_FOUND)
   # TivaCMake not in an install prefix, this means tiva-cmake has not been installed and we expected the installation to be in the local directory
-  set(CMAKE_PREFIX_PATH "${CMAKE_CURRENT_LIST_DIR}/../lib/build/install/")
-  find_package(TivaCMake REQUIRED)
+  set(CMAKE_PREFIX_PATH "${CMAKE_CURRENT_LIST_DIR}/../lib/build/install/" CACHE FILEPATH "List of locations to search for yyyConfig.cmake with find_package")
+  find_package(TivaToolchain REQUIRED)
 endif()  
 
 
@@ -19,9 +19,9 @@ endif()
 # This is because toolchain files must usually be specified either relative to the source directory or absolute
 if(CMAKE_TOOLCHAIN_FILE)
   if((NOT (EXISTS ${CMAKE_TOOLCHAIN_FILE})) AND (NOT EXISTS ${CMAKE_CURRENT_SOURCE_DIR}/${CMAKE_TOOLCHAIN_FILE}))
-    set(CMAKE_TOOLCHAIN_FILE "${TivaCMake_DIR}/${CMAKE_TOOLCHAIN_FILE}" CACHE FILEPATH "The toolchain file" FORCE)
+    set(CMAKE_TOOLCHAIN_FILE "${TivaToolchain_DIR}/${CMAKE_TOOLCHAIN_FILE}" CACHE FILEPATH "The toolchain file" FORCE)
   endif()
 endif()
 
-set(CMAKE_TOOLCHAIN_FILE "${TivaCMake_DIR}/${DEFAULT_TOOLCHAIN}" CACHE FILEPATH "The default toolchain file")
+set(CMAKE_TOOLCHAIN_FILE "${TivaToolchain_DIR}/${DEFAULT_TOOLCHAIN}" CACHE FILEPATH "The default toolchain file")
 
